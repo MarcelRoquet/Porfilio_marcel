e929783b00f5d52c0bc62b18a486adf09f28aee5
Marcel Roquet <17marcelroquet@gmail.com>
2025-04-30 12:57:10 +0200

    colormode

--- main.js
+++ main.js
@@ -7,3 +7,59 @@ burguerMenu.addEventListener('click', function () {
     menu.classList.toggle('open');
     burguerMenu.classList.toggle('open');
 });
+
+
+/*section*/ 
+const sections = document.querySelectorAll("main section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      
+      sections.forEach((sec) => sec.classList.remove("active"));
+     
+      entry.target.classList.add("active");
+    }
+  });
+}, {
+  threshold: 1
+});
+
+
+sections.forEach((section) => observer.observe(section));
+
+
+/*color mode*/
+
+const modo = document.getElementById('toggle-color-mode'); /*activar oscuro*/
+const restmodo = document.getElementById('reset-color-mode') /* activar claro*/ 
+
+const temaGuardado = localStorage.getItem('tema');
+
+if (temaGuardado === 'oscuro') {
+  document.body.classList.add('oscuro');
+  modo.style.display = 'none';
+  restmodo.style.display = 'block';
+} else {
+  document.body.classList.add('claro');
+  restmodo.style.display = 'none';
+  modo.style.display = 'block';
+} 
+
+modo.addEventListener('click', () => {
+  document.body.classList.remove('claro');
+  document.body.classList.toggle('oscuro');
+  localStorage.setItem('tema', 'oscuro');
+
+  modo.style.display = 'none'
+  restmodo.style.display = 'block'
+});
+
+restmodo.addEventListener('click', () => {
+  document.body.classList.remove('oscuro');
+  document.body.classList.toggle('claro');
+  localStorage.setItem('tema', 'claro');
+
+  restmodo.style.display = 'none'
+  modo.style.display = 'block'
+});
